601b737d516d0c83801843391a6d6407e9028130
Julien Carayon <juliencarayon@outlook.com>
2022-01-26 17:38:25 +0100

    modifié :         platformio.ini 	supprimé :        src/bq769x0.cpp 	supprimé :        src/bq769x0.h 	modifié :         src/main.cpp 	modifié :         src/main.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 #include <limits.h>
-#include <EEPROM.h>
-#include <PinChangeInterrupt.h>
+// #include <EEPROM.h>
+//#include <PinChangeInterrupt.h>
 
-#include <avr/sleep.h>
-#include <avr/power.h>
-#include <avr/wdt.h>
-
-#include "bq769x0.h"
+// #include <avr/sleep.h>
+// #include <avr/power.h>
+// #include <avr/wdt.h>
 #include "main.h"
+//#include "bq769x0.h"
+
 
 M365BMS g_M365BMS;
 BMSSettings g_Settings;
 
 bool g_Debug = true;
 // I2CAddress = 0x08, crcEnabled = true
-bq769x0 g_BMS(bq76940, 0x08, true);
+//bq769x0 g_BMS(bq76940, 0x08, true);
 volatile bool g_interruptFlag = false;
 unsigned long g_lastActivity = 0;
 unsigned long g_lastUpdate = 0;
@@ -33,7 +33,7 @@ volatile unsigned int g_timer2Overflows = 0;
 
 void alertISR()
 {
-    g_BMS.setAlertInterruptFlag();
+    //g_BMS.setAlertInterruptFlag();
     g_interruptFlag = true;
     g_wakeupFlag = true;
 }
@@ -60,126 +60,15 @@ ISR(TIMER2_OVF_vect)
 
 void setup()
 {
-    MCUSR = 0;
-    wdt_disable();
-
-    Serial.begin(76800);
+    Serial.begin(115200);
     Serial.println(F("BOOTED!"));
-
-    power_adc_disable();
-    power_spi_disable();
-    power_timer1_disable();
-    power_twi_disable();
     delay(1000);
-
-    loadSettings();
-
-    g_BMS.begin(BMS_BOOT_PIN);
-    g_BMS.setThermistors(0b110);
-
-    applySettings();
-
-    // Bluetooth power pin
-    pinMode(BMS_VDD_EN_PIN, OUTPUT);
-
-    // attach ALERT interrupt
-    pinMode(BMS_ALERT_PIN, INPUT);
-    attachPCINT(digitalPinToPCINT(BMS_ALERT_PIN), alertISR, RISING);
-
-#ifdef K1SWITCH
-    // attach K1 / power switch interrupt
-    pinMode(BMS_K1_PIN, INPUT);
-    attachPCINT(digitalPinToPCINT(BMS_K1_PIN), K1ISR, CHANGE);
-#endif
-
-    // attach UART RX pin interrupt to wake from deep sleep
-    attachPCINT(digitalPinToPCINT(0), uartRxISR, CHANGE);
-    disablePCINT(digitalPinToPCINT(0));
-
-    interrupts();
-
-    delay(1000);
-    g_BMS.update();
-    g_BMS.resetSOC(100);
-
-#ifdef K1SWITCH
-    g_dischargeEnabled = !digitalRead(BMS_K1_PIN);
-#endif
-
-    g_BMS.enableCharging();
-    if(g_dischargeEnabled)
-        g_BMS.enableDischarging();
-
-    g_Debug = false;
-
-    wdt_enable(WDTO_1S);
 }
-
-
-void loadSettings()
-{
-    if(EEPROM.read(0) != g_Settings.header[0] || EEPROM.read(1) != g_Settings.header[1])
-    {
-        for(uint16_t i = 0; i < EEPROM.length(); i++) {
-            EEPROM.write(i, 0);
-        }
-        EEPROM.put(0, g_Settings);
-    }
-    else
-    {
-        EEPROM.get(0, g_Settings);
-    }
-}
-
-void saveSettings()
-{
-    EEPROM.put(0, g_Settings);
-}
-
-void applySettings()
-{
-    g_BMS.setBatteryCapacity(g_Settings.capacity, g_Settings.nominal_voltage, g_Settings.full_voltage);
-
-    g_BMS.setShuntResistorValue(g_Settings.shuntResistor_uOhm);
-    g_BMS.setThermistorBetaValue(g_Settings.thermistor_BetaK);
-
-    g_BMS.setTemperatureLimits(g_Settings.temp_minDischargeC,
-                             g_Settings.temp_maxDischargeC,
-                             g_Settings.temp_minChargeC,
-                             g_Settings.temp_maxChargeC);
-    g_BMS.setShortCircuitProtection(g_Settings.SCD_current, g_Settings.SCD_delay);
-    g_BMS.setOvercurrentChargeProtection(g_Settings.OCD_current, g_Settings.OCD_delay);
-    g_BMS.setOvercurrentDischargeProtection(g_Settings.ODP_current, g_Settings.ODP_delay);
-    g_BMS.setCellUndervoltageProtection(g_Settings.UVP_voltage, g_Settings.UVP_delay);
-    g_BMS.setCellOvervoltageProtection(g_Settings.OVP_voltage, g_Settings.OVP_delay);
-
-    g_BMS.setBalancingThresholds(g_Settings.balance_minIdleTime,
-                               g_Settings.balance_minVoltage,
-                               g_Settings.balance_maxVoltageDiff);
-    g_BMS.setIdleCurrentThreshold(g_Settings.idle_currentThres);
-    if(g_Settings.balance_enabled)
-        g_BMS.enableAutoBalancing();
-    else
-        g_BMS.disableAutoBalancing();
-
-    g_BMS.setBalanceCharging(true);
-
-    g_BMS.adjADCPackOffset(g_Settings.adcPackOffset);
-    g_BMS.adjADCCellsOffset(g_Settings.adcCellsOffset);
-
-    strncpy(g_M365BMS.serial, g_Settings.serial, sizeof(g_M365BMS.serial));
-    g_M365BMS.design_capacity = g_M365BMS.real_capacity = g_Settings.capacity;
-    g_M365BMS.nominal_voltage = g_Settings.nominal_voltage;
-    g_M365BMS.date = g_Settings.date;
-    g_M365BMS.num_cycles = g_Settings.num_cycles;
-    g_M365BMS.num_charged = g_Settings.num_charged;
-}
-
 
 void onNinebotMessage(NinebotMessage &msg)
 {
     // Enable TX
-    UCSR0B |= (1 << TXEN0);
+    //UCSR0B |= (1 << TXEN0);
 
     if(msg.addr != M365BMS_RADDR)
         return;
@@ -194,16 +83,28 @@ void onNinebotMessage(NinebotMessage &msg)
 
         if(sz > sizeof(NinebotMessage::data))
             return;
-
+            
         msg.addr = M365BMS_WADDR;
         msg.length = 2 + sz;
 
         if(msg.mode == 0x01)
         {
-            if((ofs + sz) > sizeof(g_M365BMS))
+            if((ofs + sz) > sizeof(g_M365BMS)){
                 return;
-
+            }   
+            Serial.print("date : ");
+            Serial.println(g_M365BMS.date);
+            
+            Serial.println("before memcpy");
+            for(int i=0;i<=253;i++){
+                Serial.print(msg.data[i],HEX);
+            }
             memcpy(&msg.data, &((uint8_t *)&g_M365BMS)[ofs], sz);
+            Serial.println("after memcpy");
+            for(int i=0;i<=253;i++){
+                Serial.print(msg.data[i],HEX);
+            }
+            
         }
         else if(msg.mode == 0xF1)
         {
@@ -212,8 +113,9 @@ void onNinebotMessage(NinebotMessage &msg)
 
             memcpy(&msg.data, &((uint8_t *)&g_Settings)[ofs], sz);
         }
-
+    
         ninebotSend(msg);
+
     }
     else if(msg.mode == 0x03 || msg.mode == 0xF3)
     {
@@ -239,56 +141,10 @@ void onNinebotMessage(NinebotMessage &msg)
     {
         switch(msg.offset)
         {
-            case 1: {
-                applySettings();
-            } break;
-            case 2: {
-                EEPROM.get(0, g_Settings);
-            } break;
-            case 3: {
-                EEPROM.put(0, g_Settings);
-            } break;
-#if BQ769X0_DEBUG
-            case 4: {
-                g_Debug = msg.data[0];
-            } break;
-            case 5: {
-                debug_print();
-            } break;
-#endif
-            case 6: {
-                g_BMS.disableDischarging();
-                g_BMS.disableCharging();
-            } break;
-            case 7: {
-                g_BMS.enableDischarging();
-                g_BMS.enableCharging();
-            } break;
-
-            case 8: {
-                digitalWrite(BMS_VDD_EN_PIN, LOW);
-            } break;
-            case 9: {
-                digitalWrite(BMS_VDD_EN_PIN, HIGH);
-            } break;
-#if BQ769X0_DEBUG
-            case 10: {
-                // test watchdog
-                for (;;) { (void)0; }
-            } break;
-#endif
-            case 11: {
-                // restart to bootloader
-                typedef void (*do_reboot_t)(void);
-                const do_reboot_t do_reboot = (do_reboot_t)((FLASHEND - 511) >> 1);
-                wdt_disable();
-                cli(); TCCR0A = TCCR1A = TCCR2A = 0; // make sure interrupts are off and timers are reset.
-                MCUSR = 0;
-                do_reboot();
-            }
+            } 
         }
     }
-}
+
 
 void ninebotSend(NinebotMessage &msg)
 {
@@ -300,13 +156,26 @@ void ninebotSend(NinebotMessage &msg)
     Serial.write(msg.addr);
     Serial.write(msg.mode);
     Serial.write(msg.offset);
+    Serial.println("SEND BEGIN");
+    Serial.println(msg.header[0],HEX);
+    Serial.println(msg.header[1],HEX);
+    Serial.println(msg.length,HEX);
+    Serial.println(msg.addr,HEX);
+    Serial.println(msg.mode,HEX);
+    Serial.println(msg.offset),HEX;
+    Serial.println("SEND ENDS");
     for(uint8_t i = 0; i < msg.length - 2; i++)
     {
         Serial.write(msg.data[i]);
         msg.checksum += msg.data[i];
+        
+        Serial.print("MSG SENT : ");
+        Serial.print(msg.data[i],HEX);
+        Serial.println();
     }
 
     msg.checksum ^= 0xFFFF;
+   
     Serial.write(msg.checksum & 0xFF);
     Serial.write((msg.checksum >> 8) & 0xFF);
 }
@@ -329,7 +198,8 @@ void ninebotRecv()
 
         uint8_t byte = Serial.read();
         recvd++;
-
+        // Serial.println(byte,HEX);
+        
         switch(recvd)
         {
             case 1:
@@ -337,7 +207,10 @@ void ninebotRecv()
                 if(byte != 0x55)
                 { // header1 mismatch
                     recvd = 0;
+                    Serial.print("header1 mismatch :");
+                    Serial.println(byte,HEX);
                     break;
+                   
                 }
 
                 msg.header[0] = byte;
@@ -349,7 +222,10 @@ void ninebotRecv()
                 if(byte != 0xAA)
                 { // header2 mismatch
                     recvd = 0;
+                    Serial.print("header2 mismatch :");
+                    Serial.println(byte,HEX);
                     break;
+                    
                 }
 
                 msg.header[1] = byte;
@@ -360,6 +236,8 @@ void ninebotRecv()
                 if(byte < 2)
                 { // too small
                     recvd = 0;
+                    Serial.print("too small msg ");
+                    Serial.println(byte,HEX);
                     break;
                 }
 
@@ -372,6 +250,7 @@ void ninebotRecv()
                 if(byte != M365BMS_RADDR)
                 { // we're not the receiver of this message
                     recvd = 0;
+                    Serial.println("wrong adress");
                     break;
                 }
 
@@ -383,12 +262,16 @@ void ninebotRecv()
             {
                 msg.mode = byte;
                 checksum += byte;
+                Serial.print("mode: ");
+                Serial.println(msg.mode,HEX);
             } break;
 
             case 6: // offset
             {
                 msg.offset = byte;
                 checksum += byte;
+                Serial.print("offset: ");
+                Serial.println(msg.offset, HEX);
             } break;
 
             default:
@@ -397,10 +280,14 @@ void ninebotRecv()
                 { // data
                     msg.data[recvd - 7] = byte;
                     checksum += byte;
+                    // Serial.print("data: ");
+                    // Serial.println(byte,HEX);
                 }
                 else if(recvd - 7 - msg.length + 2 == 0)
                 { // checksum LSB
                     msg.checksum = byte;
+                    // Serial.print("checksum: ");
+                    // Serial.println(byte);
                 }
                 else
                 { // checksum MSB and transmission finished
@@ -411,8 +298,14 @@ void ninebotRecv()
                     { // invalid checksum
                         recvd = 0;
                         break;
+                        Serial.println("invalid checksum");
                     }
-
+                    
+                  
+                    for(int i = 0 ;i <= 253; i++){
+                        Serial.print(msg.data[i],HEX);
+                    }
+                    
                     onNinebotMessage(msg);
                     recvd = 0;
                 }
@@ -424,147 +317,11 @@ void ninebotRecv()
 
 void loop()
 {
-    unsigned long now = millis();
-#ifdef K1SWITCH
-    if(g_K1Flag)
-    {
-        g_K1Flag = false;
-        g_dischargeEnabled = !digitalRead(BMS_K1_PIN);
-
-        if(g_dischargeEnabled)
-            g_BMS.enableDischarging();
-        else
-            g_BMS.disableDischarging();
-    }
-#endif
-
-    if(g_interruptFlag || (unsigned long)(now - g_lastUpdate) >= 500)
-    {
-        if(g_interruptFlag)
-            g_interruptFlag = false;
-
-        uint8_t error = g_BMS.update(); // should be called at least every 250 ms
-        g_lastUpdate = now;
-
-        // update M365BMS struct
-        {
-            // charging state
-            if(g_BMS.getBatteryCurrent() > (int16_t)g_Settings.idle_currentThres)
-                g_M365BMS.status |= (1 << 6); // charging
-            else if(g_BMS.getBatteryCurrent() < (int16_t)g_Settings.idle_currentThres / 2)
-                g_M365BMS.status &= ~(1 << 6);
-
-            if(error & STAT_OV) {
-                g_M365BMS.status |= (1 << 9); // overvoltage
-                error &= ~STAT_OV;
-            }
-            else
-                g_M365BMS.status &= ~(1 << 9);
-
-            uint16_t batVoltage = g_BMS.getBatteryVoltage() / 10;
-            if(batVoltage > g_M365BMS.max_voltage)
-                g_M365BMS.max_voltage = batVoltage;
-
-            int16_t batCurrent = g_BMS.getBatteryCurrent() / 10;
-            if(batCurrent > 0 && (uint16_t)batCurrent > g_M365BMS.max_charge_current)
-                g_M365BMS.max_charge_current = batCurrent;
-            else if(batCurrent < 0 && (uint16_t)-batCurrent > g_M365BMS.max_discharge_current)
-                g_M365BMS.max_discharge_current = -batCurrent;
-
-            g_M365BMS.capacity_left = g_M365BMS.design_capacity * g_BMS.getSOC() / 100.0;
-            g_M365BMS.percent_left = g_BMS.getSOC();
-            g_M365BMS.current = -batCurrent;
-            g_M365BMS.voltage = batVoltage;
-            g_M365BMS.temperature[0] = g_BMS.getTemperatureDegC(1) + 20.0;
-            g_M365BMS.temperature[1] = g_BMS.getTemperatureDegC(2) + 20.0;
-
-            if(g_BMS.getHighestTemperature() > (g_Settings.temp_maxDischargeC - 3) * 10)
-                g_M365BMS.status |= (1 << 10); // overheat
-            else
-                g_M365BMS.status &= ~(1 << 10);
-
-            if(g_BMS.batCycles_) {
-                g_M365BMS.num_cycles += g_BMS.batCycles_;
-                g_BMS.batCycles_ = 0;
-                g_Settings.num_cycles = g_M365BMS.num_cycles;
-                EEPROM.put(0, g_Settings);
-            }
-
-            if(g_BMS.chargedTimes_) {
-                g_M365BMS.num_charged += g_BMS.chargedTimes_;
-                g_Settings.num_charged = g_M365BMS.num_charged;
-                g_BMS.chargedTimes_ = 0;
-            }
-
-            uint8_t numCells = g_BMS.getNumberOfConnectedCells();
-            for(uint8_t i = 0; i < numCells; i++)
-                g_M365BMS.cell_voltages[i] = g_BMS.getCellVoltage(i);
-
-            // cell voltage difference too big
-            uint16_t bigDelta = g_BMS.getMaxCellVoltage() - g_BMS.getMinCellVoltage();
-            if(bigDelta > 100)
-                error = 1;
-
-            if(error)
-                g_M365BMS.status &= ~1;
-            else
-                g_M365BMS.status |= 1;
-        }
-
-        if(g_oldMillis > now)
-            g_millisOverflows++;
-        g_oldMillis = now;
-    }
-
-    ninebotRecv();
-
-    if((unsigned long)(now - g_lastActivity) >= 5000 && !g_Debug)
-    {
-        // Disable TX
-        UCSR0B &= ~(1 << TXEN0);
-
-        // go into deep sleep, will wake up every 250ms by BQ769x0 ALERT or from USART1 RX (first byte will be lost)
-        noInterrupts();
-        set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-
-        // Timer/Counter2 8-byte OVF 8MHz /1024 = 32.64ms
-        TCCR2A = 0;
-        TCCR2B = (1<<CS22)|(1<<CS21)|(1<<CS20);
-        TCNT2 = 0;
-        TIMSK2 = (1<<TOIE2);
-
-        UCSR0B &= ~(1 << RXEN0); // Disable RX
-        enablePCINT(digitalPinToPCINT(0));
-
-        wdt_reset();
-        g_wakeupFlag = false;
-
-        sleep_enable();
-        interrupts();
-        do // go to sleep if it's just timer2 that woke us up (unless we were idle for longer than 500ms)
-        {
-            sleep_cpu();
-        } while(!g_wakeupFlag && g_timer2Overflows < 16);
-        sleep_disable();
-
-        // Disable Timer/Counter2 and add elapsed time to Arduinos 'timer0_millis'
-        TCCR2B = 0;
-        TIMSK2 = 0;
-        float elapsed_time = g_timer2Overflows * 32.64 + TCNT2 * 32.64 / 255.0;
-        timer0_millis += (unsigned long)elapsed_time;
-        g_timer2Overflows = 0;
-
-        if(g_uartRxInterrupted)
-            g_lastActivity = millis();
-        g_uartRxInterrupted = false;
-
-        disablePCINT(digitalPinToPCINT(0));
-        UCSR0B |= (1 << RXEN0); // Enable RX
-
-        interrupts();
-    }
-
-    wdt_reset();
+  ninebotRecv();
+//   uint8_t byte = Serial.read();
+//   if (byte != 0xFF){
+//      Serial.println(byte,HEX);
+//   }
 }
 
 #if BQ769X0_DEBUG
--- src/main.h
+++ src/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdint.h>
-#include "bq769x0.h"
+//#include "bq769x0.h"
 
 /* Software I2C */
 #define I2C_TIMEOUT 100
@@ -92,29 +92,29 @@ struct M365BMS
 { // little endian
 /*00-1F*/   uint16_t unk1[16] = {0x5A, 0x5A, 0x00};
 /*20-2D*/   char serial[14] = "";
-/*2E-2F*/   uint16_t version = 0x900; // 0x115 = 1.1.5
-/*30-31*/   uint16_t design_capacity = 0; // mAh
-/*32-33*/   uint16_t real_capacity = 0; // mAh
-/*34-35*/   uint16_t nominal_voltage = 0; // mV
-/*36-37*/   uint16_t num_cycles = 0;
-/*38-39*/   uint16_t num_charged = 0;
-/*3A-3B*/   uint16_t max_voltage = 0; // V/100
-/*3C-3D*/   uint16_t max_discharge_current = 0; // A/100
-/*3E-3F*/   uint16_t max_charge_current = 0; // A/100
-/*40-41*/   uint16_t date = 0; // MSB (7 bits year, 4 bits month, 5 bits day) LSB
+/*2E-2F*/   uint16_t version = 0x115; // 0x115 = 1.1.5
+/*30-31*/   uint16_t design_capacity = 7600; // mAh
+/*32-33*/   uint16_t real_capacity = 7500; // mAh
+/*34-35*/   uint16_t nominal_voltage = 36000; // mV
+/*36-37*/   uint16_t num_cycles = 3;
+/*38-39*/   uint16_t num_charged = 2;
+/*3A-3B*/   uint16_t max_voltage = 4200; // V/100
+/*3C-3D*/   uint16_t max_discharge_current = 3000; // A/100
+/*3E-3F*/   uint16_t max_charge_current = 200; // A/100
+/*  40-41  */   uint16_t date = (18 << 9) | (10 << 5) | 1;  // MSB (7 bits year, 4 bits month, 5 bits day) LSB
 /*42-47*/   uint8_t errors[6] = {0};
 /*48-5F*/   uint16_t unk3[12] = {0};
-/*60-61*/   uint16_t status = 1; // b0 = config valid, b6 = charging, b9 = overvoltage, b10 = overheat
-/*62-63*/   uint16_t capacity_left = 0; // mAh
-/*64-65*/   uint16_t percent_left = 0;
-/*66-67*/   int16_t current = 0; // A/100
-/*68-69*/   uint16_t voltage = 0; // V/100
-/*6A-6B*/   uint8_t temperature[2] = {0, 0}; // °C - 20
+/*60-61*/   uint16_t status = 0xb0; // b0 = config valid, b6 = charging, b9 = overvoltage, b10 = overheat
+/*62-63*/   uint16_t capacity_left = 1000; // mAh
+/*64-65*/   uint16_t percent_left = 15;
+/*66-67*/   int16_t current = 2; // A/100
+/*68-69*/   uint16_t voltage = 3100; // V/100
+/*6A-6B*/   uint8_t temperature[2] = {20, 20}; // °C - 20
 /*6C-6D*/   uint16_t balance_bits = 0;
 /*6E-75*/   uint16_t unk5[4] = {0};
 /*76-77*/   uint16_t health = 100; // %, <60% = battery bad
 /*78-7F*/   uint16_t unk6[4] = {0};
-/*80-9D*/   uint16_t cell_voltages[15] = {0}; // mV
+/*80-9D*/   uint16_t cell_voltages[15] = {3100,3100,3100,3100,3100,3100,3100,3100,3100,3100,0,0,0,0,0}; // mV
 /*9E-A1*/   uint16_t unk7[2] = {0};
 #if 0
 /*A2-A3*/   uint16_t unk8 = 1; // 1 ?
@@ -154,9 +154,10 @@ void ninebotRecv();
 void debug_print();
 
 extern bool g_Debug;
-extern bq769x0 g_BMS;
+//extern bq769x0 g_BMS;
 extern volatile bool g_interruptFlag;
 extern unsigned long g_lastActivity;
 extern unsigned long g_lastUpdate;
 
 #endif // MAIN_H
+
